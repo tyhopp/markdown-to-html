3246092253404807edec3e4d4645adde204cb5d8
Ty Hopp <hopp.ty.c@gmail.com>
2021-11-24 13:35:52 +0800

    feat: Initial server implementation

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import createServer, { IPureHttpServer } from 'pure-http';
+import { parseBody } from './lib/parse-body.js';
+import { parseJSON } from './lib/parse-json.js';
+import { PureHttpRequest, PureHttpResponse } from './types/pure-http.js';
+
+const server: IPureHttpServer = createServer();
+
+// TODO - Confirm CORS/access control requirements
+// TODO - Allow different port via process.env
+// TODO - Handle 405
+
+server.post('/markdown-to-html', async (request: PureHttpRequest, response: PureHttpResponse) => {
+  let body = await parseBody(request, response);
+  let json = await parseJSON(body, response);
+  // TODO - Validate input
+  // TODO - Transform markdown
+  // TODO - Sanitize HTML
+  let html = { html: '<h1>Hello, world!</h1>' }; // Stub for now
+  response.json(html, false, 200);
+});
+
+server.listen(3000);
+
+console.log('Listening on http://localhost:3000');
